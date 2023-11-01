6414e4b479f3b8b04e9a9114035cea2f021aafba
LeynnerRoque <111515763+LeynnerRoque@users.noreply.github.com>
2023-11-01 14:47:20 -0300

    testes unitarios

---
+++ src/test/java/br/com/alura/curso/service/ConsultasServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.alura.curso.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsultasServiceTest {
+
+    @Test
+    void agendarConsulta() {
+
+    }
+}
